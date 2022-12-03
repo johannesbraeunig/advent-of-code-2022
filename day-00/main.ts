59ce3ed6d744cc56408692e39d11f400b8c8db23
Johannes Bräunig <johannes.braeunig@xing.com>
2022-12-03 11:19:09 +0100

    add dummy files

---
+++ day-00/main.ts
@@ -0,0 +1,27 @@
+/**
+ * Advent of Code - Day 0
+ * https://adventofcode.com
+ *
+ * Run with `deno run --allow-read  main.ts`.
+ */
+
+const getParsedInput = (input: string) =>
+	input.split('\n\n').map((cluster) =>
+		cluster.split('\n').filter((el) => !!el).map((n) => parseInt(n))
+	);
+
+const getPart1 = () => 'Part 1';
+const getPart2 = () => 'Part 2';
+
+const input = await Deno.readTextFile('./input.txt');
+const parsedInput = getParsedInput(input);
+
+const part1 = getPart1();
+console.log(
+	`🍩 ${part1}`,
+);
+
+const part2 = getPart2();
+console.log(
+	`🍩 ${part2}`,
+);
